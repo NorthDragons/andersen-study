e10850ad360e4564331ebd2ad4530943d3547157
Ivan <ivankaminskii323@gmail.com>
2022-10-25 20:50:15 +0200

    structural and behavior patterns

---
+++ study/src/main/java/com/study/andersen/gof/structural/facade/codec/MPEG4CompressionCodec.java
@@ -0,0 +1,8 @@
+package com.study.andersen.gof.structural.facade.codec;
+
+import com.study.andersen.gof.structural.facade.codec.api.Codec;
+
+public class MPEG4CompressionCodec implements Codec {
+    public String type = "mp4";
+
+}
