578abf8bdeff39c1821f820cfb7879eed227a96b
Ivan <ivankaminskii323@gmail.com>
2022-10-26 20:35:07 +0200

    strategy changes

--- study/src/main/java/com/study/andersen/gof/behavior/strategy/strategy/ConsoleStrategyImpl.java
+++ study/src/main/java/com/study/andersen/gof/behavior/strategy/strategy/ConsoleStrategyImpl.java
@@ -1,34 +1,19 @@
 package com.study.andersen.gof.behavior.strategy.strategy;
 
 import com.study.andersen.gof.behavior.strategy.strategy.api.WriteStrategy;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class ConsoleStrategyImpl implements WriteStrategy {
-    private final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     private final StringBuilder finalMessage = new StringBuilder();
 
     @Override
-    public void writeTheMessage(String message) {
-        collectMailDetails();
-        finalMessage.append(message);
+    public void writeTheMessage(String message, String sender, String recipient) {
+        collectMailDetails(sender, recipient, message);
         System.out.println(finalMessage);
     }
 
-    private void collectMailDetails() {
-        String sender = "";
-        String recipient = "";
-        try {
-            System.out.println("Please enter sender's name");
-            sender = reader.readLine();
-            System.out.println("Please enter recipient's name");
-            recipient = reader.readLine();
-        } catch (IOException e) {
-            System.out.println("Data entry error");
-        }
+    private void collectMailDetails(String sender, String recipient, String message) {
         finalMessage.append("Sender: ").append(sender)
                 .append("\nRecipient: ").append(recipient)
-                .append("\n");
+                .append("\n").append(message);
     }
 }
--- study/src/main/java/com/study/andersen/gof/behavior/strategy/strategy/FileWriterStrategyImpl.java
+++ study/src/main/java/com/study/andersen/gof/behavior/strategy/strategy/FileWriterStrategyImpl.java
@@ -1,20 +1,16 @@
 package com.study.andersen.gof.behavior.strategy.strategy;
 
 import com.study.andersen.gof.behavior.strategy.strategy.api.WriteStrategy;
-import java.io.BufferedReader;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 
 public class FileWriterStrategyImpl implements WriteStrategy {
-    private final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     private final StringBuilder finalMessage = new StringBuilder();
 
     @Override
-    public void writeTheMessage(String message) {
-        collectMailDetails();
-        finalMessage.append(message);
+    public void writeTheMessage(String message, String sender, String recipient) {
+        collectMailDetails(sender, recipient, message);
         try (OutputStream fos = new FileOutputStream("./message")) {
             fos.write(finalMessage.toString().getBytes(), 0, finalMessage.length());
         } catch (IOException ex) {
@@ -22,19 +18,11 @@ public class FileWriterStrategyImpl implements WriteStrategy {
         }
     }
 
-    private void collectMailDetails() {
-        String sender = "";
-        String recipient = "";
-        try {
-            System.out.println("Please enter sender's name");
-            sender = reader.readLine();
-            System.out.println("Please enter recipient's name");
-            recipient = reader.readLine();
-        } catch (IOException e) {
-            System.out.println("Data entry error");
-        }
+    private void collectMailDetails(String sender, String recipient, String message) {
         finalMessage.append("Sender: ").append(sender)
                 .append("\nRecipient: ").append(recipient)
-                .append("\n");
+                .append("\n").append(message);
     }
+
+
 }
