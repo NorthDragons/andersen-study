6970647db8f0f068e8ceb6e21a13e61866f1da36
Ivan <ivankaminskii323@gmail.com>
2022-10-26 20:35:07 +0200

    strategy changes

--- study/src/main/java/com/study/andersen/gof/behavior/strategy/strategy/ConsoleStrategyImpl.java
+++ study/src/main/java/com/study/andersen/gof/behavior/strategy/strategy/ConsoleStrategyImpl.java
@@ -1,12 +1,8 @@
 package com.study.andersen.gof.behavior.strategy.strategy;
 
 import com.study.andersen.gof.behavior.strategy.strategy.api.WriteStrategy;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class ConsoleStrategyImpl implements WriteStrategy {
-    private final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     private final StringBuilder finalMessage = new StringBuilder();
 
     @Override
